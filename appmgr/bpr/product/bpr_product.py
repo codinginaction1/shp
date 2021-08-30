ba95314a30642cc2e98eae47f381b6f0e433f6c2
codinginaction1 <codinginaction1@gmail.com>
2021-08-30 00:59:37 +0000

    Modify using dao.

--- appmgr/bpr/product/bpr_product.py
+++ appmgr/bpr/product/bpr_product.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 import psycopg2
 import psycopg2.extras
+from shp.lib.basic.pgsql import Pgsql
 
 
 bpr_product = Blueprint('product', __name__, url_prefix='/product', template_folder='../../templates/product')
@@ -8,15 +9,7 @@ bpr_product = Blueprint('product', __name__, url_prefix='/product', template_fol
 
 @bpr_product.route('/product_manage_list')
 def product_manage_list():
-    cf = {
-        'host': 'database-2.cdyzomagbni2.ap-northeast-2.rds.amazonaws.com',
-        'port': 5432,
-        'user': 'postgres',
-        'password': 'your_password',
-        'dbname': 'db_shp_main'
-    }
-    conn = psycopg2.connect(host=cf['host'], port=cf['port'], user=cf['user'], password=cf['password'], dbname=cf['dbname'])
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    dao = Pgsql('db_shp_main')
     query = f"""
 SELECT
     product_id,
@@ -24,8 +17,8 @@ SELECT
     product_price
 FROM test_product_info
 """
-    cursor.execute(query)
-    result = cursor.fetchall()
+    dao.execute(query)
+    result = dao.fetchall()
     return render_template('product_manage_list.html', result=result)
 
 
