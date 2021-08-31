1f2acc9d85ff20673323823a5ca76ffffe2dd10f
codinginaction1 <codinginaction1@gmail.com>
2021-08-31 14:41:00 +0000

    Add before_request, teardown_request.

--- appmgr/shp_appmgr.py
+++ appmgr/shp_appmgr.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from shp.appmgr.bpr.product.bpr_product import bpr_product
 from shp.appmgr.bpr.order.bpr_order import bpr_order
+from shp.lib.basic.log import Log
 
 
 app = Flask(__name__)
@@ -14,6 +15,33 @@ app.register_blueprint(bpr_order)
 def index():
     result = {'__name__': __name__}
     return render_template('index/index.html', result=result)
+
+
+@app.before_request
+def before_request():
+    # Only the routes registered by app.route are logged.
+    # The static folder and nonexistent routes are excluded.
+    path = request.path
+    if not path.startswith('/static/'):
+        try:
+            if path == '/':
+                url = url_for('index')
+            else:
+                url = url_for(path.lstrip('/').replace('/', '.'))
+        except Exception as err:
+            return 'Invalid endpoint!', 404
+        else:
+            Log.set_logger()
+            Log.logging_access()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    # If you register the set_logger function in app.before_request,
+    # when another request comes in after one request,
+    # the previous logger value is maintained and log file handlers are continuously added.
+    # The same log is written more and more times, so the created file handler must be removed.
+    Log.unset_logger()
 
 
 if __name__ == '__main__':
