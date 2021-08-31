3d2c85735dafc876dc83eb0458943fe657a1ce15
codinginaction1 <codinginaction1@gmail.com>
2021-08-31 08:28:07 +0000

    Modify comment in English

--- lib/basic/log.py
+++ lib/basic/log.py
@@ -14,7 +14,7 @@ class Log:
     def set_logger(name=''):
         if name == '':
             if request:
-                # 너무 하위 경로로 구분하지 않고 상위 경로 두 레벨까지만 사용한다.
+                # Use only two levels of upper path, not too many levels of lower path.
                 path = re.sub('^(/\w+/\w+).*$', '\\1', request.path)
                 name = request.script_root + path
                 if name[-1:] == '/':
