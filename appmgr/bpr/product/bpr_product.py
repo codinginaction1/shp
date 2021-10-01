f1717c215f70ddcd2f92d9099876bdc2fd5bc891
codinginaction1 <codinginaction1@gmail.com>
2021-10-01 02:51:43 +0000

    Add a new route, product_manage_list_0.

--- appmgr/bpr/product/bpr_product.py
+++ appmgr/bpr/product/bpr_product.py
@@ -5,6 +5,11 @@ from shp.appmgr.blo.product.blo_product_manage_list import Blo_product_manage_li
 bpr_product = Blueprint('product', __name__, url_prefix='/product', template_folder='../../templates/product')
 
 
+@bpr_product.route('/product_manage_list_0')
+def product_manage_list_0():
+    return render_template('product_manage_list_0.html')
+
+
 @bpr_product.route('/product_manage_list')
 def product_manage_list():
     result = Blo_product_manage_list().result
