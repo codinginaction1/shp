cf8a4aa9be9122f77ac4d4e91a0846489944b35d
codinginaction1 <codinginaction1@gmail.com>
2021-10-31 05:23:21 +0000

    Add route for product_manage_list_1

--- appmgr/bpr/product/bpr_product.py
+++ appmgr/bpr/product/bpr_product.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from shp.appmgr.blo.product.blo_product_manage_list import Blo_product_manage_list
+from shp.appmgr.blo.product.blo_product_manage_list_1 import Blo_product_manage_list_1
 
 
 bpr_product = Blueprint('product', __name__, url_prefix='/product', template_folder='../../templates/product')
@@ -10,10 +10,10 @@ def product_manage_list_0():
     return render_template('product_manage_list_0.html')
 
 
-@bpr_product.route('/product_manage_list')
-def product_manage_list():
-    result = Blo_product_manage_list().result
-    return render_template('product_manage_list.html', result=result)
+@bpr_product.route('/product_manage_list_1', methods=['POST'])
+def product_manage_list_1():
+    result = Blo_product_manage_list_1().result
+    return render_template('product_manage_list_1.html', result=result)
 
 
 @bpr_product.route('/product_category_list')
